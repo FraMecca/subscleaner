9609a2960d1f12e4ca21b2174945e0f0136b65b9
Francesco Mecca <me@francescomecca.eu>
2020-01-31 13:13:58 +0100

    release including setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Subscleaner',
+      version='0.0.1',
+      description='Remove advertisement from subtitle files',
+      author='Francesco mecca',
+      license='GPL 3',
+      author_email='me@francescomecca.eu',
+      url='francescomecca.eu',
+      scripts=['subscleaner']
+)
